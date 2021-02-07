ac6b801c2a1da1d2f38118c7223d4c0a4a5fa7af
Quanwei <573011406@qq.com>
2021-02-07 14:52:42 +0800

    GLM & assimp

---
+++ demo/main.cpp
@@ -0,0 +1,17 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+
+int main(int argc, const char** argv)
+{
+
+
+
+	return 0;
+}
